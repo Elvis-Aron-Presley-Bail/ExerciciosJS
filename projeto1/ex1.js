2f6b7a334fa91201575932d3007caf09c49d53f4
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2024-11-08 12:04:29 -0300

    aprendendo JS

--- projeto1/ex1.js
+++ projeto1/ex1.js
@@ -14,14 +14,24 @@ function adicionar(){
 
     }
 }
-numeros.sort()
+
 function finalizar(){
 
-    
+    numeros.sort()
+
+    let maior = numeros[0]
+    let menor = numeros[0]
+
+    for (let pos in numeros){
+        if (numeros[pos] > maior)
+            maior = numeros[pos]
+        if (numeros[pos] < menor)
+            menor = numeros[pos]
+    }
 
     res.innerHTML += `Ao todo, temos ${numeros.length} números cadastrados. <br>`
-    res.innerHTML += `O menor valor informado foi ${numeros[0]} <br>`
-    res.innerHTML += `O maior valor informado foi ${numeros[numeros.length - 1]} <br>`
+    res.innerHTML += `O menor valor informado foi ${menor} <br>`
+    res.innerHTML += `O maior valor informado foi ${maior} <br>`
     res.innerHTML += `Somando todos os valores, temos <br>`
     res.innerHTML += `A média dos valores digitados é`
 }
