378fe115458477c42bf797a675a36b5c92cabb4c
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2024-11-15 11:48:00 -0300

    Exercicio com JS

---
+++ lampada/lampada.js
@@ -0,0 +1,16 @@
+var lampada = document.getElementById('lampada')
+
+function quebrar(){
+    lampada.style.backgroundColor = 'black'
+}
+
+function ligar(){
+    lampada.style.backgroundColor = 'yellow'
+}
+
+function desligar(){
+    lampada.style.backgroundColor = 'gray'
+}
+
+
+
