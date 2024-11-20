f47c810f17d32ed4f510885c83082e12acfded89
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2024-11-20 12:00:13 -0300

    praticando JS

---
+++ cursojs/ex1.js
@@ -0,0 +1,12 @@
+var num1 = document.getElementById('num1')
+var res  = document.getAnimations('res')
+
+function adicioanar(){
+
+    numero = Number(num1.value)
+
+    if (numero >= '100' && numero < '0'){
+        alert('numero invalido')
+    }
+
+}
