94f4a7dbba1add666e47b6bd9f2494cad373954a
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2024-11-26 12:09:37 -0300

    praticando JS criando um semáforo

---
+++ semafora/sema.js
@@ -0,0 +1,20 @@
+var vermelho = document.getElementById('vermelho')
+var amarelo = document.getElementById('amarelo')
+var verde = document.getElementById('verde')
+
+
+function verme(){
+    vermelho.style.backgroundColor = 'red'
+}
+
+function amare(){
+    amarelo.style.backgroundColor = 'yellow'
+}
+
+function verdd(){
+    verde.style.backgroundColor = 'greenyellow'
+}
+
+if (verme() = true){
+    amare() = false
+}
