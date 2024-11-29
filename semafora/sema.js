37f6322d38f4d918619d0d86450ab36d0d872679
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2024-11-28 21:30:05 -0300

    Praticando JS

--- semafora/sema.js
+++ semafora/sema.js
@@ -1,20 +1,23 @@
-var vermelho = document.getElementById('vermelho')
-var amarelo = document.getElementById('amarelo')
-var verde = document.getElementById('verde')
+var res = document.getElementById('res')
+
+let colocacao = 
+
+switch(colocacao){
+    case 1:
+        res.innerHTML = `Primeiro lugar`
+        break
+    case 2:
+        res.innerHTML = `Segundo lugar`
+        break
+    case 3:
+        res.innerHTML = `Terceiro lugar`
+        break
+    case 4: case 5: case 6:
+        res.innerHTML = `Premio de participaçao`
+        break
+    default:
+        res.innerHTML = `Nao subiu ao podio`
+        break
 
 
-function verme(){
-    vermelho.style.backgroundColor = 'red'
-}
-
-function amare(){
-    amarelo.style.backgroundColor = 'yellow'
-}
-
-function verdd(){
-    verde.style.backgroundColor = 'greenyellow'
-}
-
-if (verme() = true){
-    amare() = false
 }
