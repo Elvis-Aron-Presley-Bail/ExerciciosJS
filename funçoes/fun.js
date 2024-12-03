20ce170d7155b1e81c20e57743fd79161e626fea
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2024-12-03 12:07:01 -0300

    aprendendo funções em JS

--- funçoes/fun.js
+++ funçoes/fun.js
@@ -1,39 +1,35 @@
 var res = document.getElementById('res')
-var num = [10 , 20 , 50 , 40 , 30]
 
-let maior = 0
-let soma = 0
+let fun = function(v1,v2){
+    return v1 + v2
+}
 
-for(n in num){
-    if(maior < num[n]){
-        maior = num[n]
+res.innerHTML = fun(10,20)
+
+let funct = function(...valores){
+    let valor = 0
+    for(v of valores){
+        valor += v
     }
-    n++
+    return valor
 }
 
-for(m in num){
-    soma += num[m]
-    m++
-}
+res.innerHTML += funct(10,20,30,40)
 
-res.innerHTML = `O maior numero e o ${maior} <br>`
-res.innerHTML += `A soma de tudo e ${soma}<br>`
+let funflexa = (n1,n2) => {return n1 + n2}
 
-function mais(n1=0,n2=0){
-    let somaa = n1 + n2
-    res.innerHTML += `O resultado de ${n1} + ${n2} = ${somaa}`
-}
+res.innerHTML += funflexa(25,30)
 
-mais(15 , 30)
 
-function varios(...algo){
-    let calcu = 0
-    let resul = algo.length
-    for(va in algo){
-        calcu += algo[va]
+const soma = (...valoress)=>{
+    const somar=val=>{
+        let res = 0
+        for(va of val){
+            res+=va
+        }
+        return res
     }
-    return calcu
+    return somar(valoress)
 }
 
-res.innerHTML = varios(10 , 20 , 30 , 40 , 50 , 60)
-
+res.innerHTML = soma(10,20,30,45)
