4afe8d5eb98fd93fe6290c0d6ac8217b7547f6f8
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2025-01-15 10:36:53 -0300

    praticando JS

--- addevenlistner/add3.js
+++ addevenlistner/add3.js
@@ -3,17 +3,35 @@ const caixa2 = document.getElementById('caixa2')
 const caixas = [...document.querySelectorAll('.caixa')]
 const botaoCopiar = document.getElementById('btn_copiar')
 const botaoVoltar = document.getElementById('btn_voltar')
+const cursos = ['HTML' , 'CSS' , 'JS']
 
 caixas.map((el) =>{
-    el.addEventListener('click', (evt)=>{
+    el.addEventListener('click', (evt) =>{
         const marcado = evt.target
         marcado.classList.toggle('selecionado')
     })
 })
 
-botaoCopiar.addEventListener('click', () => {
-    const cursoSelecionado = [...document.querySelectorAll('.selecionado')]
-    cursoSelecionado.map((el)=>{
-        caixa2.appendChild(el)
+botaoCopiar.addEventListener('click', (evt) =>{
+    const selecionado = [...document.querySelectorAll('.selecionado')]
+    selecionado.map((sele) =>{
+        caixa2.appendChild(sele)
     })
+})
+
+botaoVoltar.addEventListener('click', (evt) =>{
+    const naoSelecionado = [...document.querySelectorAll('.curso:not(.selecionado)')]
+    naoSelecionado.map((el) =>{
+        caixa1.appendChild(el)
+    })
+})
+
+
+cursos.map((el) =>{
+    const novoElemento = document.createElement('div')
+    novoElemento.innerHTML = el
+    novoElemento.setAttribute('id' , 'c1')
+    novoElemento.setAttribute('class' , 'curso c1')
+
+    caixa1.appendChild(novoElemento)
 })
